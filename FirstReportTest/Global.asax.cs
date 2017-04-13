d310c6a40274d26a0048cfd9b5356a1de9f69c6f
v-yuanhu <v-yuanhu@HW-YUANHU-01>
2017-04-13 16:27:42 +0800

    testCommit

--- FirstReportTest/Global.asax.cs
+++ FirstReportTest/Global.asax.cs
@@ -13,7 +13,6 @@ namespace FirstReportTest
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
         }
     }
 }
